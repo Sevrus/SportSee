131dbae1a449edce3531334290042889468a7441
Sevrus <book-n-troll@nordnet.fr>
2025-03-17 12:55:35 +0100

    Add fetchUserData service with mock and API support
    This commit introduces a fetchUserData function to retrieve user data from either mock files or a live API, depending on the useMockData flag. It supports multiple endpoints and includes error handling for both mock and API scenarios.

---
+++ src/services/fetchUserData.js
@@ -0,0 +1,28 @@
+const useMockData = true;
+
+const fetchUserData = async (endpoint, userId) => {
+    if (useMockData) {
+        const mockData = await import("public/mocks/mockUserStats.json");
+        switch (endpoint) {
+            case 'USER_MAIN_DATA':
+                return mockData.USER_MAIN_DATA.find(user => user["id"] === userId);
+            case 'USER_ACTIVITY':
+                return mockData.USER_ACTIVITY.find(activity => activity["userId"] === userId);
+            case 'USER_AVERAGE_SESSIONS':
+                return mockData.USER_AVERAGE_SESSIONS.find(avg => avg["userId"] === userId);
+            case 'USER_PERFORMANCE':
+                return mockData.USER_PERFORMANCE.find(performance => performance["userId"] === userId);
+            default:
+                throw new Error('Endpoint not found in mock data.');
+        }
+    } else {
+        const URL_BASE = 'https://api.exemple.com/';
+        const response = await fetch(`${URL_BASE}${endpoint}/${userId}`);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data: ${response.statusText}`);
+        }
+        return response.json();
+    }
+};
+
+export default fetchUserData;
